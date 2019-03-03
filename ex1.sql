614f1be9169fcd95f2ba79774b01f4312f93b229
OnkelDunkel <rasbalnor@gmail.com>
2019-03-03 11:23:38 +0100

    finished ex3

--- ex1.sql
+++ ex1.sql
@@ -1,23 +1,12 @@
-USE stackoverflow;
-DROP PROCEDURE IF EXISTS denormalizeComments;
+use stackoverflow;
+DROP TRIGGER IF EXISTS after_comment_insert;
+DROP PROCEDURE IF EXISTS insertComment;
 DELIMITER $$
-CREATE PROCEDURE `denormalizeComments` (in postId int(11))
+CREATE PROCEDURE `insertComment` (IN id int(11),IN postId int(11),IN userId int(11))
 BEGIN
-UPDATE posts SET
-CommentsJsonArr = (
-	SELECT JSON_ARRAYAGG(jsonComments.jsonObj) from (
-		SELECT JSON_OBJECT(
-			'Id', Id,
-			'PostId', PostId,
-			'Score', Score,
-			'Text', Text,
-			'CreationDate', CreationDate,
-			'UserId', UserId
-		) AS jsonObj
-		FROM comments
-		WHERE PostId = postId
-	) AS jsonComments
-)
-WHERE ID = postId;
+	INSERT INTO comments
+	(`Id`, `PostId`, `UserId`)
+	VALUES (id, postId, userId);
+	CALL denormalizeComments(postId);
 END $$
 DELIMITER ;
